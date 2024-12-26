99ffcb0aa2112434e8ae18ab246a8ab4abe44f80
Eugene <eugene.kasap@gmail.com>
2024-12-26 21:41:07 +0100

    Added integration test

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
     //runtimeOnly("org.postgresql:postgresql")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-webflux")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
@@ -44,4 +45,12 @@ kotlin {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+sourceSets {
+    test {
+        java {
+            srcDirs(listOf("src/test/kotlin/integration", "src/test/kotlin/unit"))
+        }
+    }
 }
