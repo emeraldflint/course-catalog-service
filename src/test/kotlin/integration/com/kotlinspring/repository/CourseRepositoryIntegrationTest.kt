9292baf43ca0a3a4feb024ac8bbb1aa7cda43770
Eugene <eugene.kasap@gmail.com>
2024-12-28 21:33:47 +0100

    Retrieve Courses by name using JPA Query Creation Function

---
+++ src/test/kotlin/integration/com/kotlinspring/repository/CourseRepositoryIntegrationTest.kt
@@ -0,0 +1,33 @@
+package com.kotlinspring.repository
+
+import com.kotlinspring.util.courseEntityList
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.test.context.ActiveProfiles
+import kotlin.test.assertEquals
+
+@DataJpaTest
+@ActiveProfiles("test")
+class CourseRepositoryIntegrationTest {
+
+    @Autowired
+    lateinit var courseRepository: CourseRepository
+
+    @BeforeEach
+    fun setUp() {
+        courseRepository.deleteAll()
+        val courses = courseEntityList()
+        courseRepository.saveAll(courses)
+    }
+
+    @Test
+    fun retrieveAllCourses() {
+        val courses = courseRepository.findByNameContaining("SpringBoot")
+
+        assertEquals(2, courses.size)
+    }
+
+
+}
