089c5d1c20c6f36948426e874450012ee9cdcc8f
Eugene <eugene.kasap@gmail.com>
2024-12-28 22:08:49 +0100

    Added test for multiple sets of Data using @Parameterized test

--- src/test/kotlin/integration/com/kotlinspring/repository/CourseRepositoryIntegrationTest.kt
+++ src/test/kotlin/integration/com/kotlinspring/repository/CourseRepositoryIntegrationTest.kt
@@ -3,9 +3,13 @@ package com.kotlinspring.repository
 import com.kotlinspring.util.courseEntityList
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.test.context.ActiveProfiles
+import java.util.stream.Stream
 import kotlin.test.assertEquals
 
 @DataJpaTest
@@ -36,5 +40,22 @@ class CourseRepositoryIntegrationTest {
         assertEquals(2, courses.size)
     }
 
+    @ParameterizedTest
+    @MethodSource("courseAndSize")
+    fun findCourseByName_approach2(name: String, expectedSize: Int) {
+        val courses = courseRepository.findCourseByName(name)
 
+        assertEquals(expectedSize, courses.size)
+
+    }
+
+    companion object {
+        @JvmStatic
+        fun courseAndSize(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.arguments("SpringBoot", 2),
+                Arguments.arguments("Wiremock", 1)
+            )
+        }
+    }
 }
