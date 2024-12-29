0abc25d1f6104dd27542b689947fb5e8b924e7ac
Eugene <eugene.kasap@gmail.com>
2024-12-29 14:58:52 +0100

    Added the relationship in the Entity Class

--- src/main/kotlin/com/kotlinspring/entity/Course.kt
+++ src/main/kotlin/com/kotlinspring/entity/Course.kt
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
 
 
@@ -14,8 +16,11 @@ data class Course(
     @GeneratedValue(strategy = GenerationType.AUTO)
     val id: Int?,
     var name: String,
-    var category: String
+    var category: String,
+    @ManyToOne(fetch = jakarta.persistence.FetchType.LAZY)
+    @JoinColumn(name = "INSTRUCTOR_ID", nullable = false)
+    var instructor: Instructor
 ) {
     // No-args constructor
-    constructor() : this(null, "", "")
+    constructor() : this(null, "", "", Instructor())
 }
